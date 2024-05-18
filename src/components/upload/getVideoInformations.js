85689f34a5c34ab5dbffad9792b19c945900a5e7
pedroorf <pedroorf@gmail.com>
2024-05-18 17:38:08 -0300

    wip

---
+++ src/components/upload/getVideoInformations.js
@@ -0,0 +1,34 @@
+import { type } from "@testing-library/user-event/dist/type";
+
+export const getVideoInformations = async (file) => {
+  const fileCallbackToPromise = (fileObj) => {
+    return Promise.race([
+      new Promise((resolve) => {
+        if (fileObj instanceof HTMLImageElement) fileObj.onload = resolve;
+        else fileObj.onloadedmetadata = resolve;
+      }),
+      new Promise((_, reject) => {
+        setTimeout(reject, 1000);
+      }),
+    ]);
+  };
+
+  const originalName = file.name
+    .split(".")
+    .slice(0, -2)
+    .join(".");
+
+  const objectUrl = URL.createObjectURL(file);
+  // const isVideo = type.startsWith('video/');
+  const video = document.createElement("video");
+  video.src = objectUrl;
+  await fileCallbackToPromise(video);
+  return {
+    duration: video.duration,
+    width: video.videoWidth,
+    height: video.videoHeight,
+    originalName,
+    type: file.type,
+    size: file.size,
+  };
+}
--- src/components/upload/index.jsx
+++ src/components/upload/index.jsx
@@ -5,10 +5,14 @@ import { fetchFile, toBlobURL } from "@ffmpeg/util";
 // import { compressAndConvertToHLS as compress } from "../compress/compress";
 
 import { uploadFileToS3 } from "./UploadFileToS3";
+import { getVideoInformations } from "./getVideoInformations";
 
 function Upload() {
   const [fileSelected, setFileSelected] = useState({ type: "", name: "" });
+  const [fileBuffer, setFileBuffer] = useState(null);
   const [fileUrl, setFileUrl] = useState(null);
+  const [fileDownloadUrl, setFileDownloadUrl] = useState(null);
+  const [fileInputUrl, setFileInputUrl] = useState(null);
   const fileInput = React.useRef();
   const [loaded, setLoaded] = useState(false);
   const ffmpegRef = useRef(new FFmpeg());
@@ -19,20 +23,21 @@ function Upload() {
     const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
     const ffmpeg = ffmpegRef.current;
     ffmpeg.on("log", ({ message }) => {
-      messageRef.current.innerHTML = message;
       console.log(message);
     });
     // toBlobURL is used to bypass CORS issue, urls with the same
     // domain can be used directly.
+
+    ffmpeg.on("progress", data => {
+      console.log(`Progress: ${data.time}`);
+      messageRef.current.innerText = `Progress: ${JSON.stringify(data)}`;
+    });
+
     await ffmpeg.load({
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
       wasmURL: await toBlobURL(
         `${baseURL}/ffmpeg-core.wasm`,
         "application/wasm"
-      ),
-      workerURL: await toBlobURL(
-        `${baseURL}/ffmpeg-core.worker.js`,
-        "text/javascript"
       ),
     });
     setLoaded(true);
@@ -48,9 +53,14 @@ function Upload() {
     }
   }, [loaded]);
 
-  function onChangeFile(event) {
+  const onChangeFile = async event => {
+    const file = event.target.files[0];
+    const fileBuffer = await file.arrayBuffer();
+    setFileBuffer(fileBuffer);
     setFileSelected(event.target.files[0]);
-  }
+    const videDuration = await getVideoInformations(event.target.files[0]);
+    console.log("\nvideDuration: ", videDuration);
+  };
 
   const handleClick = async event => {
     event.preventDefault();
@@ -75,42 +85,73 @@ function Upload() {
     // const videoURL =
     //   "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/video-15s.avi";
     const hashnamefile = uuidv4();
-    const outputFilePath = "../../tempvideo" + hashnamefile + ".mp4";
+    const outputFilePath = "../../tempvideo" + hashnamefile;
     console.log("\noutputFilePath", outputFilePath);
     const videoURL = fileUrl;
     const ffmpeg = ffmpegRef.current;
-    await ffmpeg.writeFile("input.avi", await fetchFile(videoURL));
+    await ffmpeg.writeFile(
+      "input.avi",
+      await fetchFile(videoRef.current?.files[0])
+    );
+
+    const currentTime = new Date();
+    const timestamp = Math.floor(currentTime.getTime() / 1000);
+    const originalName = videoRef.current?.files[0].name
+      .split(".")
+      .slice(0, -1)
+      .join(".");
+    const finalName = `${originalName}-${timestamp}`;
+
+    alert("Comprimindo e convertendo vídeo...");
+    // ffmpeg.writeFile("input.mp4", await fetchFile(fileBuffer));
+    ffmpeg.writeFile("input.mp4", new Uint8Array(fileBuffer));
+
     await ffmpeg.exec([
       "-i",
-      "input.avi",
+      "input.mp4",
       "-c:v",
       "libx264",
-      "-b:v",
-      "500k",
-      "-c:a",
-      "aac",
-      "-b:a",
-      "64k",
+      "-bsf:v",
+      "h264_mp4toannexb",
       "-hls_time",
       "2",
-      "-hls_list_size",
-      "0",
-      "-f",
-      "hls",
-      "output.mp4",
+      "-hls_playlist_type",
+      "vod",
+      `${finalName}.mp4`,
     ]);
-    const fileData = await ffmpeg.readFile("output.mp4");
+    // await ffmpeg.exec([
+    //   "-i",
+    //   "input.mp4",
+    //   "-c:v",
+    //   "libx264",
+    //   "-b:v",
+    //   "500k",
+    //   "-c:a",
+    //   "aac",
+    //   "-b:a",
+    //   "64k",
+    //   "-hls_time",
+    //   "2",
+    //   "-hls_list_size",
+    //   "0",
+    //   "-f",
+    //   "hls",
+    //   `${finalName}.mp4`,
+    // ]);
+    const fileData = await ffmpeg.readFile(`${finalName}.mp4`);
     const data = new Uint8Array(fileData);
-    if (videoRef.current) {
-      videoRef.current.src = URL.createObjectURL(
-        new Blob([data.buffer], { type: "video/mp4" })
+    if (data) {
+      setFileDownloadUrl(
+        URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }))
       );
     }
 
     // const blob = new Blob([data.buffer], { type: "video/mp4" });
     // const url = toBlobURL(blob);
-
-    console.log("Video saved to: " + fileData);
+    if (fileData) {
+      console.log("Video saved to: " + fileData);
+      videoRef.current = null;
+    }
   };
 
   if (!loaded) {
@@ -137,7 +178,13 @@ function Upload() {
           Split video to segments of 180 sec. and plays 2nd segment
         </button>
         <br />
+        <br />
         <p ref={messageRef}></p>
+        <p>
+          <a href={fileDownloadUrl} target="_blank" rel="noreferrer">
+            Download file
+          </a>
+        </p>
       </form>
     </>
   );
